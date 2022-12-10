68242ff05378d22097af5f3f180aabe66f2125cf
Michal Pawlowski <michal.pawlowski@hp.com>
2022-12-10 15:51:58 +0100

    day10

--- src/main/java/pl/lssystems/adventofcode/day10/AssignmentsTest.java
+++ src/main/java/pl/lssystems/adventofcode/day10/AssignmentsTest.java
@@ -1,25 +1,24 @@
 package pl.lssystems.adventofcode.day10;
 
 import org.junit.jupiter.api.Test;
-import pl.lssystems.adventofcode.day09.*;
 import pl.lssystems.adventofcode.utils.Utils;
 
 public class AssignmentsTest {
 
     @Test
     public void assignment1() {
-        Utils.processLine("day09/input.txt", line -> {
-            String[] instruction = line.split(" ");
-        });
-        System.out.println(": ");
+        CPU cpu = new CPU();
+        Utils.processLine("day10/input.txt", cpu::parseInstruction);
+        System.out.println("Finish Info | Cycle: " + cpu.getCycle() + ", RegisterX: " + cpu.getRegisterX());
+        System.out.println("Summary signal strength: " + cpu.getSummarySignalStrength());
     }
 
     @Test
     public void assignment2() {
-        Utils.processLine("day09/input.txt", line -> {
-            String[] instruction = line.split(" ");
-        });
-        System.out.println(": ");
+        CPU cpu = new CPU();
+        CRT crt = new CRT(cpu);
+        Utils.processLine("day10/input.txt", cpu::parseInstruction);
+        System.out.println("Current frame:\n" + crt.renderFrame());
     }
 
 }
